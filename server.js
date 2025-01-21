4cdb8aa0ee4ecb022b8d6d59812239cfb4cba998
abdelrazekAli <dev.abdelrazek@gmail.com>
2025-01-22 00:42:13 +0200

    add cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require("cors");
 const bookingRouter = require('./routers/bookingRouter');
 const tableRouter = require('./routers/tablesRouter');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 // Use routers
