717129b2404f3b911b70e361dfcef83990ef4b4a
abdelrazekAli <dev.abdelrazek@gmail.com>
2025-01-22 17:22:46 +0200

    logs all requests

--- server.js
+++ server.js
@@ -7,6 +7,25 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+// Middleware to log request details
+app.use((req, res, next) => {
+    console.log('--- New Request ---');
+    console.log('Method:', req.method);
+    console.log('URL:', req.url);
+    console.log('Headers:', req.headers);
+
+    // Log the body (if available)
+    if (req.body && Object.keys(req.body).length > 0) {
+        console.log('Body:', req.body);
+    } else {
+        console.log('Body: No body data');
+    }
+    console.log('-------------------');
+
+    next(); // Pass control to the next middleware
+});
+
 // Use routers
 app.use('/api', bookingRouter);
 app.use('/api', tableRouter);
